ef47f8904d47893f636c6ac1cb9410cccd48f0dd
kate hickey <kate.hickey26@gmail.com>
2020-02-20 03:50:05 +0000

    Chapter 7 commits, tests all OK

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -2,26 +2,29 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 class Category(models.Model):
+    NAME_MAX_LENGTH = 100
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
-    
+
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
-    
+
     class Meta:
         verbose_name_plural = 'categories'
-    
+
     def __str__(self):
         return self.name
 
 class Page(models.Model):
+    TITLE_MAX_LENGTH = 200
+    URL_MAX_LENGTH = 250
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return self.title
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
+from rango.forms import PageForm
+from django.urls import reverse
 
 def index(request):
     #return HttpResponse("Rango says hey there partner!(<a #href='/rango/about/'>About</a>)")
@@ -12,23 +16,23 @@ def index(request):
     # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    
+
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage matches to {{ boldmessage }} in the template!
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    
+
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/index.html', context=context_dict)
-    
+
 def about(request):
     #return HttpResponse("Rango says here is the about page.(<a #href='/rango/'>Index</a>)")
     return render(request, 'rango/about.html')
-    
+
 def show_category(request, category_name_slug):
 # Create a context dictionary which we can pass
 # to the template rendering engine.
@@ -38,24 +42,76 @@ def show_category(request, category_name_slug):
         # If we can't, the .get() method raises a DoesNotExist exception.
         # The .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
-        
+
         # Retrieve all of the associated pages.
         # The filter() will return a list of page objects or an empty list.
         pages = Page.objects.filter(category=category)
-        
+
         # Adds our results list to the template context under name pages.
         context_dict['pages'] = pages
         # We also add the category object from
         # the database to the context dictionary.
         # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
-   
+
     except Category.DoesNotExist:
         # We get here if we didn't find the specified category
         # Don't do anything -
         # the template will display the "no category" message for us
         context_dict['category'] = None
         context_dict['pages'] = None
-    
+
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    # if its an HTTP post...
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # if we have been provided with a valid form
+        if form.is_valid():
+            #saves new category to the database
+            form.save(commit=True)
+            # could confirm this category after its saved.
+            # instead, redirect the user back to index views
+            return redirect('/rango/')
+        else:
+            # the supplied form contained errors
+            # print them to terminal
+            print(form.errors)
+
+        # will handle the bad form, new form or no form supplied cases
+        # render the form with error messages, if any
+    return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    if category is None:
+        return redirect('/rango/')
+
+    form = PageForm()
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+
+                return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
+
+        else:
+            print(form.errors)
+
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context=context_dict)
